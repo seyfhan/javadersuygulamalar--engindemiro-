d7a7d7cd46eba7a9c803c347d66c4f21c72f4212
seyfhan <113800851+seyfhan@users.noreply.github.com>
2022-09-18 00:57:34 +0300

    Add files via upload

---
+++ javadersuygulamalar--engindemiro-/README.md
@@ -0,0 +1,1 @@
+# javadersuygulamalar--engindemiro-
---
+++ javadersuygulamalar--engindemiro-/enbuyuksayibulma/src/enbuyuksayibulma/enbuyuksayibulma.java
@@ -0,0 +1,45 @@
+package enbuyuksayibulma;
+
+public class enbuyuksayibulma {
+
+	public static void main(String[] args) {
+		
+		int sayi1 = 20;
+		int sayi2 = 35;
+		int sayi3 = 45;
+		
+		if(sayi1>sayi2) {
+			
+			if(sayi1>sayi3) {
+				
+				System.out.println(sayi1);
+				
+				
+			}
+			else {
+				System.out.println(sayi3);
+			}
+			
+			
+			
+		}
+		else {
+			if(sayi2>sayi3) {
+				
+				System.out.println(sayi2);
+			}
+			else {
+				System.out.println(sayi3);
+			}
+		
+		
+		
+		}
+		
+		
+		
+
+	
+
+
+}}
